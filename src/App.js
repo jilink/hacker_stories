4bba823d9cda20464c23b732fcb98226242d0020
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-04 17:52:22 +0100

    Search by title working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const List = (props) => {
+  return props.list.map((item) => (
+    <div key={item.objectId}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
+};
+
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan walke",
+      num_comments: 3,
+      points: 4,
+      objectId: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan truc",
+      num_comments: 2,
+      points: 5,
+      objectId: 1,
+    },
+  ];
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(function (story) {
+    return story.title.includes(searchTerm);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My hacker stories</h1>
+
+      <Search onSearch={handleSearch} />
+      <p>
+        <strong>{searchTerm}</strong>
+      </p>
+      <hr />
+      <List list={searchedStories} />
     </div>
   );
-}
+};
+
+const Search = (props) => {
+  return (
+    <React.Fragment>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={props.onSearch} />
+    </React.Fragment>
+  );
+};
 
 export default App;
