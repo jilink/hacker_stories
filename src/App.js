ebad2f38d9c37259f01d4752d29c9be8f969d98a
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-07 16:42:43 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -10,15 +10,19 @@ const List = ({ list, onRemoveItem }) =>
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <div>
-      <span>
+    <div className="item">
+      <span style={{ width: "40%" }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: "30%" }}>{item.author}</span>
+      <span style={{ width: "10%" }}>{item.num_comments}</span>
+      <span style={{ width: "10%" }}>{item.points}</span>
+      <span style={{ width: "10%" }}>
+        <button
+          type="button"
+          onClick={() => onRemoveItem(item)}
+          className="button button_small"
+        >
           Dismiss
         </button>
       </span>
@@ -40,7 +44,7 @@ const useSemiPersistentState = (key, initialState) => {
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -50,7 +54,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -133,19 +141,14 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My hacker stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My hacker stories</h1>
 
       <SearchForm
         onSearchSubmit={handleSearchSubmit}
         onSearchInput={handleSearchInput}
         searchTerm={searchTerm}
       />
-
-      <p>
-        <strong>{searchTerm}</strong>
-      </p>
-      <hr />
       {stories.isError && <p>Something went wrong ...</p>}
       {stories.isLoading ? (
         <p>Loading ... </p>
@@ -172,7 +175,9 @@ const InputWithLabel = ({
   }, [isFocused]);
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
       &nbsp;
       <input
         id={id}
@@ -180,6 +185,7 @@ const InputWithLabel = ({
         value={value}
         onChange={onInputChange}
         ref={inputRef}
+        className="input"
       />
     </>
   );
