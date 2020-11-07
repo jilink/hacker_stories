4be8f437672649707f49fecdbd758261f3c896ee
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-07 20:08:16 +0100

    with styled components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,84 @@ import logo from "./logo.svg";
 import styles from "./App.module.css";
 import React, { useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
 
 const List = ({ list, onRemoveItem }) =>
   list.map((item) => (
@@ -10,23 +88,23 @@ const List = ({ list, onRemoveItem }) =>
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <div className={styles.item}>
-      <span style={{ width: "40%" }}>
+    <StyledItem>
+      <StyledColumn width="40%">
         <a href={item.url}>{item.title}</a>
-      </span>
-      <span style={{ width: "30%" }}>{item.author}</span>
-      <span style={{ width: "10%" }}>{item.num_comments}</span>
-      <span style={{ width: "10%" }}>{item.points}</span>
-      <span style={{ width: "10%" }}>
-        <button
+      </StyledColumn>
+      <StyledColumn width="30%">{item.author}</StyledColumn>
+      <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+      <StyledColumn width="10%">{item.points}</StyledColumn>
+      <StyledColumn width="10%">
+        <StyledButtonSmall
           type="button"
           onClick={() => onRemoveItem(item)}
           className={`${styles.button} ${styles.buttonSmall}`}
         >
           Dismiss
-        </button>
-      </span>
-    </div>
+        </StyledButtonSmall>
+      </StyledColumn>
+    </StyledItem>
   );
 };
 
@@ -44,7 +122,7 @@ const useSemiPersistentState = (key, initialState) => {
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit} className={styles.SearchForm}>
+  <StyledSearchForm onSubmit={onSearchSubmit} className={styles.SearchForm}>
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -54,14 +132,14 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button
+    <StyledButtonLarge
       type="submit"
       disabled={!searchTerm}
       className={`${styles.button} ${styles.buttonLarge}`}
     >
       Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+  </StyledSearchForm>
 );
 
 const App = () => {
@@ -141,8 +219,8 @@ const App = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.headlinePrimary}>My hacker stories</h1>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My hacker stories</StyledHeadlinePrimary>
 
       <SearchForm
         onSearchSubmit={handleSearchSubmit}
@@ -155,7 +233,7 @@ const App = () => {
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </div>
+    </StyledContainer>
   );
 };
 
@@ -175,11 +253,9 @@ const InputWithLabel = ({
   }, [isFocused]);
   return (
     <>
-      <label htmlFor={id} className={styles.label}>
-        {children}
-      </label>
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
       &nbsp;
-      <input
+      <StyledInput
         id={id}
         type={type}
         value={value}
