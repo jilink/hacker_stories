639ed8973500ab5889ecade82df9aa865ea5e7d6
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-11 11:30:40 +0100

    Adding sorting function to list

--- src/List.tsx
+++ src/List.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import styles from "./App.module.css";
 import { ReactComponent as Check } from "./check.svg";
+import { sortBy } from "lodash";
 
 const StyledColumn = styled.span<{ width: string }>`
   padding: 0 5px;
@@ -41,6 +42,29 @@ const StyledButtonSmall = styled(StyledButton)`
   padding: 5px;
 `;
 
+const SortButton = styled.button`
+  cursor: pointer;
+  background: transparent;
+  box-shadow: 0px 0px 0px transparent;
+  border: 0px solid transparent;
+  text-shadow: 0px 0px 0px transparent;
+  &:focus {
+    outline: none;
+  }
+`;
+
+interface Sort {
+  [key: string]: (list: Stories) => Stories;
+}
+
+const SORTS: Sort = {
+  NONE: (list: Stories) => list,
+  TITLE: (list: Stories) => sortBy(list, "title"),
+  AUTHOR: (list: Stories) => sortBy(list, "author"),
+  COMMENT: (list: Stories) => sortBy(list, "num_comments").reverse(),
+  POINT: (list: Stories) => sortBy(list, "points").reverse(),
+};
+
 type Stories = Array<Story>;
 
 type Story = {
@@ -57,13 +81,43 @@ type ListProps = {
   onRemoveItem: (item: Story) => void;
 };
 
-const List = ({ list, onRemoveItem }: ListProps) => (
-  <>
-    {list.map((item) => (
-      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-    ))}
-  </>
-);
+const List = ({ list, onRemoveItem }: ListProps) => {
+  const [sort, setSort] = useState({ sortKey: "NONE", isReverse: false });
+  const handleSort = (sortkey: string) => {
+    const isReverse = sort.sortKey === sortkey && !sort.isReverse;
+    setSort({ sortKey: sortkey, isReverse: isReverse });
+  };
+
+  const sortFunction = SORTS[sort.sortKey];
+  const sortedList = sort.isReverse
+    ? sortFunction(list).reverse()
+    : sortFunction(list);
+
+  return (
+    <>
+      <StyledItem>
+        <StyledColumn width="40%">
+          <SortButton onClick={() => handleSort("TITLE")}>Title</SortButton>
+        </StyledColumn>
+        <StyledColumn width="30%">
+          <SortButton onClick={() => handleSort("AUTHOR")}>Author</SortButton>
+        </StyledColumn>
+        <StyledColumn width="10%">
+          <SortButton onClick={() => handleSort("COMMENT")}>
+            Comments
+          </SortButton>
+        </StyledColumn>
+        <StyledColumn width="10%">
+          <SortButton onClick={() => handleSort("POINT")}>Points</SortButton>
+        </StyledColumn>
+        <StyledColumn width="10%"></StyledColumn>
+      </StyledItem>
+      {sortedList.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      ))}
+    </>
+  );
+};
 
 type ItemProps = {
   item: Story;
